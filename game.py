64e45b9f77b7dc488597cca581d9a8bbe567ed23
sarahabimay <sarahabimay@gmail.com>
2018-09-13 14:37:03 +0100

    Refactor game.

--- game.py
+++ game.py
@@ -3,9 +3,10 @@ class Game:
 	def __init__(self, word):
 		self.word = word
 		self.lives = 7
+		self.goodGuesses = []
 
 	def show_word(self):
-		print self.word
+		print " The word was " + self.word
 
 	def show_lives(self):
 		print "You have " + str(self.lives) + " lives"
@@ -14,10 +15,8 @@ class Game:
 		letters = []
 		for character in self.word:
 			letters.append("[_]")
-
 		print "  ".join(letters)
 	
-	#letter_entered = []
 	def get_letter(self):
 		return raw_input("What is your letter?")
 	
@@ -25,33 +24,48 @@ class Game:
 		return letter in self.word
 
 	def evaluate_guess(self, guess):
-		
 		if self.is_letter_in_word(guess):
 			print "good guess!"
-			 #show the letter in the right place
+			self.goodGuesses.append(guess)
+			self.replace_all_guessed_letters()
+			self.after_good_guess()
 		else:
 			self.reduce_lives()			
-			if self.lives > 0:
-				print "Try again"
-				self.get_letter()
-			else:
-				print "Game over"
+			self.after_bad_guess()
 
-			
+	def after_good_guess(self):
+		if len(self.goodGuesses) == len(self.word):
+			print "YOU WON!!"
+		if self.lives > 0 and len(self.goodGuesses) != len(self.word): 
+			letter = self.get_letter()
+			self.evaluate_guess(letter)
+
+	def after_bad_guess(self):
+		if self.lives > 0:
+			print "Try again"
+			letter = self.get_letter()
+			self.evaluate_guess(letter)
+		else:
+			print "Game over"
+			self.show_word()
 
 	def reduce_lives(self):
 		self.lives -= 1 
-		print "you have lost a life,you now have " + str(self.lives)
+		print "you have lost a life, you now have " + str(self.lives)
 
 	def game_is_over(self):
 		self.lives == 0
 
-	#def show_letter(self):
-		#for character in self.word:
-			#letters.append(letter_entered)
+	def replace_all_guessed_letters(self):
+		convertedWord = []
+		for character in self.word:
+			if character in self.goodGuesses:
+				convertedWord.append(character)
+			else :
+				convertedWord.append("[_]")
+		print " ".join(convertedWord)
 
 our_game = Game("mouse")
-#our_game.show_word()
 our_game.show_lives()
 our_game.hide_word()
 letter = our_game.get_letter()
